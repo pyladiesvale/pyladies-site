37878960c75c89cd323dff33099e1c0379bb2083
Rodrigo M. Mariano <rodmariano13@gmail.com>
2016-02-17 17:10:57 -0200

    Adicionada nova constante: CONNECTUS.

--- pyladies/pyladies/core/constants.py
+++ pyladies/pyladies/core/constants.py
@@ -27,3 +27,21 @@ MEMBERS = [
 		}		
 	]
 ]
+
+CONNECTUS = [
+	{"link": "https://www.google.com.br/maps/place/S%C3%A3o+Jos%C3%A9+dos+Campos,+SP/@-23.1894443,-46.0030986,11z/data=!3m1!4b1!4m2!3m1!1s0x94cc4bb3858cc2e7:0xba25a33168f8c1", 
+		"icon": "fa fa-map-marker fa-2x", 
+		"description": "Vale do Paraíba - SP - Brasil"},
+	{"link": "mailto:vale@pyladies.com", 
+		"icon": "fa fa-envelope fa-2x", 
+		"description": "vale@pyladies.com"},
+	{"link": "https://github.com/pyladiesvale", 
+		"icon": "fa fa-github fa-2x", 
+		"description": "pyladiesvale"},
+	{"link": "https://twitter.com/pyladiesvale", 
+		"icon": "fa fa-twitter fa-2x", 
+		"description": "@pyladiesvale"},
+	{"link": "https://www.facebook.com/pyladiesvale", 
+		"icon": "fa fa-facebook fa-2x", 
+		"description": "fb.me/pyladiesvale"}
+]
--- pyladies/pyladies/core/views.py
+++ pyladies/pyladies/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .constants import MEMBERS
+from .constants import MEMBERS, CONNECTUS
 
 
 def home(request):
-	context = {'MEMBERS': MEMBERS}
+	context = {'MEMBERS': MEMBERS, 'CONNECTUS': CONNECTUS}
 	return render(request, 'core/index.html', context)
